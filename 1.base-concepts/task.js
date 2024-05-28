e670d071bc4d8db147ec48c5a98b3b67a69c5aac
Andrey Maximov <arman.gen40@mail.ru>
2024-05-28 21:33:31 +0300

    1 задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,11 +1,24 @@
-"use strict"
+"use strict";
 
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  let discriminant = Math.pow(b, 2) - 4 * a * c ;
+
+  if (discriminant < 0) {
+      return arr;
+  } else if (discriminant === 0) {
+      arr.push(-b / (2 * a));
+      return arr;
+  } else {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a)); 
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a)); 
+    return arr;
+  };
+
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {}
+
+console.log(solveEquation(4, 9, 2));
