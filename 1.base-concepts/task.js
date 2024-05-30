8db00c5057affa82abc2cd154f94ecb3de1cb489
Andrey Maximov <arman.gen40@mail.ru>
2024-05-30 19:44:53 +0300

    Корректировка 1 задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,12 +5,10 @@ function solveEquation(a, b, c) {
 
   let discriminant = Math.pow(b, 2) - 4 * a * c ;
 
-  if (discriminant < 0) {
-      return arr;
-  } else if (discriminant === 0) {
-      arr.push(-b / (2 * a));
-      return arr;
-  } else {
+  if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+    return arr;
+  } else if (discriminant > 0) {
     arr.push((-b + Math.sqrt(discriminant)) / (2 * a)); 
     arr.push((-b - Math.sqrt(discriminant)) / (2 * a)); 
     return arr;
