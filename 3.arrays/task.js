509f9b8e7856ef9664a1c456405081822c3e4bf0
Andrey Maximov <arman.gen40@mail.ru>
2024-06-20 19:30:46 +0300

    Arrays 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((element, index) => element === arr2[index])
+  );
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users
+    .filter((element) => element.gender === gender)
+    .map((year) => year.age)
+    .reduce((acc, item, index, arr) => {
+      acc += item;
+      if (index === arr.length - 1) {
+        return acc / arr.length;
+      }
+      return acc;
+    }, 0);
 }
